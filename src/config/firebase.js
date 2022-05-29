8a59bbb041f80cf05926b90ebbe17a16b0d16190
Anton Zavalniuk <zavalniuk.dev@gmail.com>
2022-05-29 11:38:50 +0200

    connect firestore

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -1,5 +1,6 @@
 import { initializeApp } from "firebase/app";
 import { initializeAuth, getReactNativePersistence } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
 import Constants from "expo-constants";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 
@@ -21,4 +22,7 @@ const auth = initializeAuth(app, {
   persistence: getReactNativePersistence(AsyncStorage),
 });
 
-export { auth };
+// initialize firestore
+const db = getFirestore();
+
+export { auth, db };
--- src/screens/HomeScreen/index.js
+++ src/screens/HomeScreen/index.js
@@ -1,16 +1,39 @@
 import { View, Text, Button } from "react-native";
 import { signOut } from "firebase/auth";
+import { useState, useEffect } from "react";
+import {
+  collection,
+  query,
+  orderBy,
+  onSnapshot,
+  addDoc,
+} from "firebase/firestore";
 
 import { styles } from "./styles";
-import { auth } from "../../config/firebase";
+import { auth, db } from "../../config/firebase";
 
 export const HomeScreen = () => {
+  const [restaurants, setRestaurants] = useState([]);
+
   const handleLogout = () => {
     signOut(auth).catch((error) => console.log("Error logging out: ", error));
   };
+
+  useEffect(() => {
+    const restaurantsRef = collection(db, "restaurants");
+    const q = query(restaurantsRef, orderBy("id"));
+
+    const unsubscribe = onSnapshot(q, (querySnapshot) => {
+      setRestaurants(querySnapshot.docs.map((doc) => doc.data()));
+    });
+  }, []);
+
+  console.log("restaurants", restaurants);
+
   return (
     <View style={styles.container}>
       <Text>Home Screen</Text>
+
       <Button title="Sign Out" onPress={handleLogout} />
     </View>
   );
