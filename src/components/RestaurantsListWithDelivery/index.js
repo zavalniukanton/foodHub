9dbc8dd599a58a25260fd50f01eaac0e4f01ad23
Anton Zavalniuk <zavalniuk.dev@gmail.com>
2022-05-31 18:40:44 +0200

    add card component to restaurant list with delivery

--- src/components/RestaurantsListWithDelivery/index.js
+++ src/components/RestaurantsListWithDelivery/index.js
@@ -1,6 +1,9 @@
 import { Text, FlatList, Pressable, Image } from "react-native";
 
 import { styles } from "./styles";
+import { View } from "../View";
+import { Icon } from "../Icon";
+import { Colors } from "../../theme/colors";
 
 export const RestaurantsListWithDelivery = ({ data }) => {
   return (
@@ -13,7 +16,59 @@ export const RestaurantsListWithDelivery = ({ data }) => {
           <Pressable style={styles.card}>
             <Image source={{ uri: item.image }} style={styles.image} />
 
-            <Text style={styles.buttonText}>{item.name}</Text>
+            <View style={styles.cardContent}>
+              <View style={styles.titleSection}>
+                <Text style={styles.restaurantName}>{item.name}</Text>
+                <Icon
+                  name="star"
+                  color={Colors.orange}
+                  size={18}
+                  style={{ marginLeft: "auto", marginRight: 4 }}
+                />
+                <Text style={styles.rating}>{item.rating}</Text>
+                <Text style={styles.review}>({item.review})</Text>
+              </View>
+
+              <View style={styles.categories}>
+                {item.category.map((category) => (
+                  <Text key={category} style={styles.category}>
+                    {category}
+                  </Text>
+                ))}
+              </View>
+
+              <View style={styles.deliveryInfo}>
+                <Icon
+                  name="clock"
+                  color={Colors.gray}
+                  size={18}
+                  style={styles.deliveryInfoIcon}
+                />
+                <Text style={styles.deliveryInfoText}>
+                  {item.deliveryMinTime} - {item.deliveryMaxTime} min
+                </Text>
+
+                <Icon
+                  name="bicycle"
+                  color={Colors.gray}
+                  size={18}
+                  style={styles.deliveryInfoIcon}
+                />
+                <Text style={styles.deliveryInfoText}>
+                  {item.deliveryPrice} zł
+                </Text>
+
+                <Icon
+                  name="cart"
+                  color={Colors.gray}
+                  size={18}
+                  style={styles.deliveryInfoIcon}
+                />
+                <Text style={styles.deliveryInfoText}>
+                  Min. {item.minOrder} zł
+                </Text>
+              </View>
+            </View>
           </Pressable>
         );
       }}
--- src/components/RestaurantsListWithDelivery/styles.js
+++ src/components/RestaurantsListWithDelivery/styles.js
@@ -4,7 +4,7 @@ import { Colors } from "../../theme/colors";
 
 export const styles = StyleSheet.create({
   card: {
-    marginBottom: 40,
+    marginBottom: 20,
     borderRadius: 10,
     borderWidth: 1,
     borderColor: Colors.gray,
@@ -17,10 +17,58 @@ export const styles = StyleSheet.create({
     },
     shadowOpacity: 0.25,
     shadowRadius: 4,
-    elevation: 25,
+    elevation: 5,
   },
   image: {
     width: "100%",
-    height: 75,
+    height: 150,
+  },
+  cardContent: {
+    paddingHorizontal: 10,
+    paddingVertical: 12,
+  },
+  titleSection: {
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
+  },
+  restaurantName: {
+    fontWeight: "700",
+    fontSize: 18,
+    color: Colors.black,
+  },
+  rating: {
+    marginRight: 10,
+    fontWeight: "700",
+    fontSize: 14,
+    color: Colors.orange,
+  },
+  review: {
+    fontWeight: "400",
+    fontSize: 14,
+    color: Colors.black,
+  },
+  categories: {
+    marginTop: 6,
+    flexDirection: "row",
+  },
+  category: {
+    marginRight: 10,
+    fontWeight: "400",
+    fontSize: 14,
+    color: Colors.gray,
+    textDecorationLine: "underline",
+  },
+  deliveryInfo: {
+    marginTop: 12,
+    flexDirection: "row",
+    alignItems: "center",
+  },
+  deliveryInfoIcon: { marginRight: 4 },
+  deliveryInfoText: {
+    marginRight: 10,
+    fontWeight: "400",
+    fontSize: 15,
+    color: Colors.gray,
   },
 });
